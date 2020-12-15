76d56ba65825e13c6d006102970121a30d5cb385
Lucas Lopatka <lucas@Lucass-MacBook-Pro.local>
2020-12-14 23:38:20 -0800

    More changes.

--- src/components/list-items/components/list-items.tsx
+++ src/components/list-items/components/list-items.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from '@material-ui/core';
+import {Box, List} from '@material-ui/core';
 import './list-items.scss'
 
 import { ListItem } from './list-item';
@@ -9,24 +9,19 @@ export interface ListItemsProps {
 
 // eslint-disable-next-line no-empty-pattern
 export const ListItems = ({}: ListItemsProps) => {
+    const numberOfItems = 50
+    const items = Array.from(new Array(numberOfItems), (i) => ({
+        description: 'Testing testing 123',
+        order: i + 1
+    }));
+
+    const listItems = items.map((item) =>
+        <Box key={item.order} m={2}><ListItem /></Box>
+    );
+
     return (
         <div className="list-items">
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
-            <Box m={2}><ListItem /></Box>
+            {listItems}
         </div>
     )
 }
